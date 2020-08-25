b142f68a0d9004da0ae90da0c9d10fb3165eb91f
Hiroki Fujii <hfujii@hisystron.com>
2020-08-25 15:56:39 +0900

    新しいViewBaseの適用

--- views/base.py
+++ views/base.py
@@ -3,6 +3,7 @@
 #Copyright (C) 2019 Yukio Nozawa <personal@nyanchangames.com>
 #Copyright (C) 2020 yamahubuki <itiro.ishino@gmail.com>
 
+import _winxptheme
 import wx
 import constants
 import keymap
@@ -18,16 +19,24 @@ class BaseView(object):
 	"""ビューの基本クラス。"""
 	def __init__(self):
 		self.shortcutEnable=True
+		self.viewMode=globalVars.app.config.getstring("view","colorMode","white",("normal","dark"))
 
-	def Initialize(self, ttl, x, y,px,py):
+	def Initialize(self, ttl, x, y,px,py,style=wx.DEFAULT_FRAME_STYLE):
 		"""タイトルとウィンドウサイズとポジションを指定して、ウィンドウを初期化する。"""
-		self.hFrame=wx.Frame(None,-1,ttl, size=(x,y),pos=(px,py))
+		self.hFrame=wx.Frame(None,-1,ttl, size=(x,y),pos=(px,py),name=ttl,style=style)
+		_winxptheme.SetWindowTheme(self.hFrame.GetHandle(),"","")
 		self.hFrame.Bind(wx.EVT_MOVE_END,self.events.WindowMove)
 		self.hFrame.Bind(wx.EVT_SIZE,self.events.WindowResize)
 		self.hFrame.Bind(wx.EVT_CLOSE,self.events.Exit)
+		self.MakePanel()
 
+	def MakePanel(self):
 		self.hPanel=views.ViewCreator.makePanel(self.hFrame)
 		self.creator=views.ViewCreator.ViewCreator(1,self.hPanel,None, wx.VERTICAL)
+
+	def Clear(self):
+		self.hFrame.DestroyChildren()
+		self.MakePanel()
 
 	def Show(self):
 		self.creator.GetPanel().Layout()
@@ -68,45 +77,99 @@ class BaseView(object):
 
 class BaseMenu(object):
 	def __init__(self,identifier):
-		"""acceleratorTable登録準備"""
-		self.keymap=keymap.KeymapHandler(defaultKeymap.defaultKeymap)
+		"""メニューバー・acceleratorTable登録準備"""
+		self.hMenuBar=wx.MenuBar()
+		self.keymap=keymap.KeymapHandler(defaultKeymap.defaultKeymap,keymap.KeyFilter().SetDefault(False,True))
 		self.keymap_identifier=identifier
 		self.keymap.addFile(constants.KEYMAP_FILE_NAME)
 		errors=self.keymap.GetError(identifier)
 		if errors:
-			tmp=_(constants.KEYMAP_FILE_NAME+"で設定されたショートカットキーが正しくありません。キーが重複しているか、存在しないキー名を指定しています。以下のキーの設定内容をご確認ください。\n\n")
+			tmp=_(constants.KEYMAP_FILE_NAME+"で設定されたショートカットキーが正しくありません。キーの重複、存在しないキー名の指定、使用できないキーパターンの指定などが考えられます。以下のキーの設定内容をご確認ください。\n\n")
 			for v in errors:
 				tmp+=v+"\n"
 			dialog(_("エラー"),tmp)
 		self.acceleratorTable=self.keymap.GetTable(self.keymap_identifier)
 
-	def RegisterMenuCommand(self,menu_handle,ref_id,title=""):
-		"""メニューに項目を追加"""
+		#これ以降はユーザ設定の追加なのでフィルタを変更
+		self.keymap.filter=keymap.KeyFilter().SetDefault(False,False)
+
+	def RegisterMenuCommand(self,menu_handle,ref_id,title="",subMenu=None,index=-1):
 		if type(ref_id)==dict:
 			for k,v in ref_id.items():
-				self._RegisterMenuCommand(menu_handle,k,v)
+				self._RegisterMenuCommand(menu_handle,k,v,None,index)
 		else:
-			return self._RegisterMenuCommand(menu_handle,ref_id,title)
+			return self._RegisterMenuCommand(menu_handle,ref_id,title,subMenu,index)
 
-	def _RegisterMenuCommand(self,menu_handle,ref_id,title):
-		"""メニューアイテム生成補助関数"""
+	def _RegisterMenuCommand(self,menu_handle,ref_id,title,subMenu,index):
+		if ref_id=="" and title=="":
+			if index>=0:
+				menu_handle.InsertSeparator()
+			else:
+				menu_handle.AppendSeparator()
+			return
 		shortcut=self.keymap.GetKeyString(self.keymap_identifier,ref_id)
 		s=title if shortcut is None else "%s\t%s" % (title,shortcut)
-		menu_handle.Append(menuItemsStore.getRef(ref_id),s)
+		if subMenu==None:
+			if index>=0:
+				menu_handle.Insert(index,menuItemsStore.getRef(ref_id),s)
+			else:
+				menu_handle.Append(menuItemsStore.getRef(ref_id),s)
+		else:
+			if index>=0:
+				menu_handle.Insert(index,menuItemsStore.getRef(ref_id),s,subMenu)
+			else:
+				menu_handle.Append(menuItemsStore.getRef(ref_id),s,subMenu)
 
-
-	def RegisterCheckItemCommand(self,menu_handle,ref_id,title):
+	def RegisterCheckMenuCommand(self,menu_handle,ref_id,title,index=-1):
 		"""チェックメニューアイテム生成補助関数"""
 		shortcut=self.keymap.GetKeyString(self.keymap_identifier,ref_id)
 		s=title if shortcut is None else "%s\t%s" % (title,shortcut)
-		menu_handle.AppendCheckItem(menuItemsStore.getRef(ref_id),s)
+		if index>=0:
+			menu_handle.InsertCheckItem(index,menuItemsStore.getRef(ref_id),s)
+		else:
+			menu_handle.AppendCheckItem(menuItemsStore.getRef(ref_id),s)
 
-	def RegisterRadioItemCommand(self,menu_handle,ref_id,title):
+	def RegisterRadioMenuCommand(self,menu_handle,ref_id,title,index=-1):
 		"""ラジオメニューアイテム生成補助関数"""
 		shortcut=self.keymap.GetKeyString(self.keymap_identifier,ref_id)
 		s=title if shortcut is None else "%s\t%s" % (title,shortcut)
-		menu_handle.AppendRadioItem(menuItemsStore.getRef(ref_id),s)
+		if index>=0:
+			menu_handle.InsertRadioItem(index,menuItemsStore.getRef(ref_id),s)
+		else:
+			menu_handle.AppendRadioItem(menuItemsStore.getRef(ref_id),s)
 
+	def CheckMenu(ref_id,state=True):
+		return self.menu.Check(menuItemsStore.getRef(ref_id),state)
+
+	def EnableMenu(self,ref_id,enable=True):
+		return self.hMenuBar.Enable(menuItemsStore.getRef(ref_id),enable)
+
+	def getItemInfo(self):
+		"""
+			メニューに登録されたすべてのアイテムを[(表示名,ref)...]で返します。
+		"""
+		ret=[]
+		print(self.hMenuBar.GetMenus())
+		print(self.hMenuBar.GetMenuCount())
+
+		if self.hMenuBar==None:
+			return ret
+		for menu,id in self.hMenuBar.GetMenus():
+			print(menu)
+			self._addMenuItemList(menu,ret)
+		return ret
+
+	def _addMenuItemList(self,menu,ret):
+		if type(menu)==wx.Menu:
+			items=menu.GetMenuItems()
+		else:
+			items=menu.GetSubMenu().GetMenuItems()
+		for item in items:
+			if item.GetSubMenu()!=None:
+				self._addMenuItemList(item,ret)
+			else:
+				if item.GetItemLabelText()!="":		#セパレータ対策
+					ret.append((item.GetItemLabelText(),item.GetId()))
 
 class BaseEvents(object):
 	"""イベント処理のデフォルトの動作をいくつか定義してあります。"""
@@ -134,5 +197,7 @@ class BaseEvents(object):
 
 			#設定ファイルに最大化状態か否かを保存
 			globalVars.app.config[self.identifier]["maximized"]=self.parent.hFrame.IsMaximized()
+
 		#sizerを正しく機能させるため、Skipの呼出が必須
 		event.Skip()
+
--- views/baseDialog.py
+++ views/baseDialog.py
@@ -12,16 +12,20 @@ class BaseDialog(object):
 	def __init__(self):
 		self.app=globalVars.app
 		self.value=None
+		self.viewMode=globalVars.app.config.getstring("view","colorMode","white",("normal","dark"))
 
-	def Initialize(self, parent,ttl):
+	def Initialize(self, parent,ttl,style=wx.DEFAULT_DIALOG_STYLE | wx.BORDER_DEFAULT):
 		"""タイトルを指定して、ウィンドウを初期化し、親の中央に配置するように設定。"""
-		self.wnd=wx.Dialog(parent,-1, ttl,style=wx.DEFAULT_DIALOG_STYLE | wx.BORDER_DEFAULT)
+		self.wnd=wx.Dialog(parent,-1, ttl,style = style)
 		_winxptheme.SetWindowTheme(self.wnd.GetHandle(),"","")
+		self.wnd.Bind(wx.EVT_CLOSE,self.OnClose)
 
 		self.panel = wx.Panel(self.wnd,wx.ID_ANY)
 		self.sizer = wx.BoxSizer(wx.VERTICAL)
 		self.panel.SetSizer(self.sizer)
 
+	#ウィンドウを中央に配置してモーダル表示する
+	#ウィンドウ内の部品を全て描画してから呼び出す
 	def Show(self, modal=True):
 		self.panel.Layout()
 		self.sizer.Fit(self.wnd)
@@ -45,9 +49,9 @@ class BaseDialog(object):
 	def GetData(self):
 		return None
 
-	#ウィンドウを中央に配置してモーダル表示する
-	#ウィンドウ内の部品を全て描画してから呼び出す
-	def ShowModal(self):
-		self.sizer.Fit(self.wnd)
-		self.wnd.Centre()
-		return self.wnd.ShowModal()
+	#closeイベントで呼ばれる。Alt+F4対策
+	def OnClose(self,event):
+		if self.wnd.GetWindowStyleFlag() | wx.CLOSE_BOX==wx.CLOSE_BOX:
+			self.wnd.Destroy()
+		else:
+			event.Veto()
