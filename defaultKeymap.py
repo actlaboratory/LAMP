e5f8dc50bed5752b015598a304eaa4e5b2d991a9
Hiroki Fujii <hfujii@hisystron.com>
2020-09-18 01:28:46 +0900

    旧LAMPの仕様を継承するショートカットキーに変更

--- defaultKeymap.py
+++ defaultKeymap.py
@@ -5,21 +5,37 @@
 defaultKeymap={}
 
 defaultKeymap["mainView"]={
+	# ファイルメニュー
+	"FILE_OPEN": "ctrl+shift+F",
+	"DIR_OPEN": "ctrl+shift+O",
+	"URL_OPEN": "ctrl+U",
+	"M3U_OPEN": "ctrl+O",
+	"NEW_M3U8_SAVE": "ctrl+shift+S",
+	"M3U8_SAVE": "ctrl+S",
+	"M3U_ADD": "ctrl+shift+L",
+	"M3U_CLOSE": "ctrl+shift+C",
+	"EXIT": "ctrl+Q",
+	# 機能メニュー
+	"SET_SLEEPTIMER": "ctrl+T",
+	"SET_EFFECTOR": "ctrl+E",
+	"ABOUT_PLAYING": "ctrl+shift+I",
+	# 操作メニュー
 	"VOLUME_UP": "ctrl+uparrow",
 	"VOLUME_DOWN": "ctrl+downarrow",
 	"VOLUME_DEFAULT": "ctrl+shift+uparrow",
 	"MUTE": "ctrl+shift+downarrow",
-	"PLAY_PAUSE": "ctrl+K",
-	"STOP": "ctrl+;",
-	"NEXT_TRACK": "ctrl+shift+L",
-	"PREVIOUS_TRACK": "ctrl+shift+J",
+	"PLAY_PAUSE": "ctrl+P/space",
+	"STOP": "ctrl+Space",
+	"NEXT_TRACK": "ctrl+F",
+	"PREVIOUS_TRACK": "ctrl+B",
 	"FAST_FORWARD": "ctrl+rightarrow",
 	"REWIND": "ctrl+leftarrow",
-	"SKIP": "ctrl+L",
-	"REVERSE_SKIP": "ctrl+J",
-	"SKIP_INTERVAL_INCREASE": "ctrl+shift+rightarrow",
-	"SKIP_INTERVAL_DECREASE": "ctrl+shift+leftarrow",
-	"REPEAT_LOOP": "ctrl+R",
+	"SKIP": "alt+leftarrow",
+	"REVERSE_SKIP": "alt+rightarrow",
+	"SKIP_INTERVAL_INCREASE": "alt+uparrow",
+	"SKIP_INTERVAL_DECREASE": "alt+downarrow",
+	"REPEAT_LOOP": "ctrl+L",
+	"SHUFFLE": "ctrl+H",
 	"EXAMPLE": "ctrl+F12/shift+delete/ctrl+c/INSERT/ctrl+BACK",
 }
 
