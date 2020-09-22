4c74078bf48f58ba9bc7abd75e5f975b544e1952
Kochi <c011610813@edu.teu.ac.jp>
2020-09-22 22:32:30 +0900

    Python3.8で起動できない問題を修正

--- Application.py
+++ Application.py
@@ -2,7 +2,6 @@
 #Application startup file
 
 import win32timezone#ダミー
-import m3uManager
 def _(string): pass#dummy
 
 #dllを相対パスで指定した時のため、カレントディレクトリを変更
@@ -12,13 +11,17 @@ os.chdir(os.path.dirname(os.path.abspath(__file__)))
 #Python3.8対応
 #dllやモジュールをカレントディレクトリから読み込むように設定
 import sys
+print(sys.version_info.major>=3 and sys.version_info.minor>=8)
+
 if sys.version_info.major>=3 and sys.version_info.minor>=8:
 	os.add_dll_directory(os.path.dirname(os.path.abspath(__file__)))
+	print(os.path.dirname(os.path.abspath(__file__)))
 	sys.path.append(os.path.dirname(os.path.abspath(__file__)))
 
 import app as application
 import constants
 import globalVars
+import m3uManager
 
 def main():
 	app=application.Main()
