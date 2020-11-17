ba9510447e0e019238079698820fa720f2cfeba6
Hiroki Fujii <hfujii@hisystron.com>
2020-11-18 01:58:29 +0900

    起動時の処理を調整

--- lamp.py
+++ lamp.py
@@ -1,19 +1,10 @@
 # -*- coding: utf-8 -*-
 #Application startup file
 
-
-#dllを相対パスで指定した時のため、カレントディレクトリを変更
-import os, sys
-os.chdir(os.path.dirname(sys.executable))
-#Python3.8対応
-#dllやモジュールをカレントディレクトリから読み込むように設定
-import sys
-if sys.version_info.major>=3 and sys.version_info.minor>=8:
-	os.add_dll_directory(os.path.dirname(os.path.abspath(__file__)))
-	sys.path.append(os.path.dirname(os.path.abspath(__file__)))
-
-import simpleDialog
-simpleDialog.dialog("debug", str(os.getcwd()) + "\n" + "\n".join(sys.argv))
+import sys, os
+#カレントディレクトリを設定
+if hasattr(sys,"frozen"): os.chdir(os.path.dirname(sys.executable))
+else: os.chdir(os.path.abspath(os.path.dirname(__file__)))
 
 #エラーの出力
 import traceback
@@ -27,21 +18,19 @@ def exchandler(type, exc, tb):
 sys.excepthook=exchandler
 
 import multiprocessing
+multiprocessing.freeze_support() #これがないとマルチプロセスでおかしなことになる
+
 import win32timezone#ダミー
 def _(string): pass#dummy
 
 #dllを相対パスで指定した時のため、カレントディレクトリを変更
-
-os.chdir(os.path.dirname(os.path.abspath(__file__)))
-
-#Python3.8対応
-#dllやモジュールをカレントディレクトリから読み込むように設定
-
-multiprocessing.freeze_support() #これがないとマルチプロセスでおかしなことになる
-
 if sys.version_info.major>=3 and sys.version_info.minor>=8:
-	os.add_dll_directory(os.path.dirname(os.path.abspath(__file__)))
-	sys.path.append(os.path.dirname(os.path.abspath(__file__)))
+	os.add_dll_directory(os.getcwd())
+	sys.path.append(os.getcwd())
+
+
+import simpleDialog
+simpleDialog.dialog("debug", str(os.getcwd()) + "\n" + "\n".join(sys.argv))
 
 import app as application
 import constants
