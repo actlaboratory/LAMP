3ac9f5cd95edaf67c32ec1673c2a1c536f1bdbb8
Hiroki Fujii <hfujii@hisystron.com>
2020-11-23 00:35:16 +0900

    バージョン情報のタイポ修正

--- views/versionDialog.py
+++ views/versionDialog.py
@@ -30,7 +30,7 @@ class dialog(baseDialog.BaseDialog):
         textList.append(_("開発元") + ": %s - %s" %(constants.APP_DEVELOPERS, constants.APP_DEVELOPERS_URL))
         textList.append(_("ソフトウェア詳細情報") + ": " + constants.APP_DETAILS_URL)
         textList.append("")
-        textList.append(_("本ソフトウェアには、他の団体または個人の成果物が含まれている場合があります。ライセンス情報の詳細については,同梱の license.txt を参照してくださtい。"))
+        textList.append(_("本ソフトウェアには、他の団体または個人の成果物が含まれている場合があります。ライセンス情報の詳細については,同梱の license.txt を参照してください。"))
         textList.append("")
         textList.append("Copyright (c) %s %s All lights reserved." %(constants.APP_COPYRIGHT_YEAR, constants.APP_DEVELOPERS))
 
