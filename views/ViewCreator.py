d9361b64ea906e1d954ae21ae56b4bccac49320f
Kochi <c011610813@edu.teu.ac.jp>
2020-11-24 18:14:44 +0900

    コード量を削減

--- views/ViewCreator.py
+++ views/ViewCreator.py
@@ -6,25 +6,18 @@
 import wx
 import win32api
 import _winxptheme
+
+import views.lampViewObject
+
 from views import ViewCreatorBase
+from views.ViewCreatorBase import *
 
-NORMAL=0
-BUTTON_COLOUR=1
-SKIP_COLOUR=2
-
-GridSizer = -1
-FlexGridSizer = -2
-
-MODE_WHITE=0
-MODE_DARK=1
-
-class ViewCreator(ViewCreatorBase.ViewCreatorBase):
+class ViewCreator(ViewCreatorBase):
 	def __init__(self, *pArg, **kArg):
-		# wxオブジェクトの入れ替えが必要ならば記述
-		# 136
-		# [object] = newObject
-
 		super().__init__(*pArg, **kArg)
+
+		# 標準オブジェクトの変更が必要ならば記述
+		# self.winObject["object_name"] = newObject
 
 	def customListCtrl(self,lcObject, text, event=None, style=0, size=(200,200), sizerFlag=wx.ALL, proportion=0,margin=5,textLayout=wx.DEFAULT):
 		hStaticText,sizer,parent=self._addDescriptionText(text,textLayout,sizerFlag, proportion,margin)
@@ -37,37 +30,3 @@ class ViewCreator(ViewCreatorBase.ViewCreatorBase):
 		Add(sizer,hListCtrl,proportion,sizerFlag,margin)
 		self.AddSpace()
 		return hListCtrl,hStaticText
-
-
-#parentで指定したsizerの下に、新たなBoxSizerを設置
-def BoxSizer(parent,orient=wx.VERTICAL,flg=0,border=0):
-	sizer=wx.BoxSizer(orient)
-	if (parent!=None):
-		parent.Add(sizer,0,flg,border)
-	return sizer
-
-def Add(sizer, window, proportion=0, flag=0, border=0, expandFlag=None):
-	if isinstance(sizer,wx.BoxSizer):
-		if sizer.Orientation==wx.VERTICAL:
-			for i in (wx.ALIGN_TOP , wx.ALIGN_BOTTOM , wx.ALIGN_CENTER_VERTICAL):
-				if flag&i==i:flag-=i
-		else:
-			for i in (wx.ALIGN_LEFT , wx.ALIGN_RIGHT , wx.ALIGN_CENTER_HORIZONTAL , wx.ALIGN_CENTER):
-				if flag&i==i:flag-=i
-	if expandFlag==wx.HORIZONTAL:	#幅を拡張
-		if type(sizer) in (wx.BoxSizer,wx.StaticBoxSizer) and sizer.GetOrientation()==wx.VERTICAL:
-			sizer.Add(window,proportion,flag | wx.EXPAND, border)
-		else:
-			sizer.Add(window,1,flag,border)
-	else:
-		sizer.Add(window,proportion,flag,border)
-
-# parentで指定されたフレームにパネルを設置する
-def makePanel(parent):
-	hPanel=wx.Panel(parent,wx.ID_ANY,size=(-1,-1))
-	return hPanel
-
-
-class TextCtrl(wx.TextCtrl):
-	def AcceptsFocusFromKeyboard(self):
-		return True
