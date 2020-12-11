11ee22dbdebb01caa2fd48e16ee593bfbd21dc08
Hiroki Fujii <hfujii@hisystron.com>
2020-12-11 19:19:16 +0900

    フォルダ割り込み追加に対応 - close #46

--- lampPipe.py
+++ lampPipe.py
@@ -16,13 +16,21 @@ def startPipeServer():
     pipeServer.start()
 
 def onReceive(msg):
-    if os.path.isfile(msg):
-        if os.path.splitext(msg)[1].lower() in globalVars.fileExpansions:
+    if os.path.exists(msg):
+        if os.path.isdir(msg) or os.path.splitext(msg)[1].lower() in globalVars.fileExpansions:
             setting = globalVars.app.config.getstring("player", "fileInterrupt", "play", ("play", "addPlaylist", "addQueue", "addQueueHead"))
-            if setting == "play": wx.CallAfter(globalVars.eventProcess.forcePlay, msg)
-            elif setting == "addPlaylist": wx.CallAfter(listManager.addItems, [msg], globalVars.app.hMainView.playlistView)
-            elif setting == "addQueue": wx.CallAfter(listManager.addItems, [msg], globalVars.app.hMainView.queueView)
-            elif setting == "addQueueHead": wx.CallAfter(listManager.addItems, [msg], globalVars.app.hMainView.queueView, 0)
+            if setting == "play":
+                if os.path.isfile(msg): wx.CallAfter(globalVars.eventProcess.forcePlay, msg)
+                else: wx.CallAfter(listManager.addItems, [msg], globalVars.app.hMainView.queueView, 0)
+            elif setting == "addPlaylist":
+                if os.path.isfile(msg): wx.CallAfter(listManager.addItems, [msg], globalVars.app.hMainView.playlistView)
+                else: wx.CallAfter(listManager.addItems, [msg], globalVars.app.hMainView.playlistView)
+            elif setting == "addQueue":
+                if os.path.isfile(msg): wx.CallAfter(listManager.addItems, [msg], globalVars.app.hMainView.queueView)
+                else: wx.CallAfter(listManager.addItems, [msg], globalVars.app.hMainView.queueView)
+            elif setting == "addQueueHead":
+                if os.path.isfile(msg): wx.CallAfter(listManager.addItems, [msg], globalVars.app.hMainView.queueView, 0)
+                else: wx.CallAfter(listManager.addItems, [msg], globalVars.app.hMainView.queueView, 0)
         elif os.path.splitext(msg)[1].lower() == ".m3u" or os.path.splitext(msg)[1].lower() == ".m3u8":
             setting = globalVars.app.config.getstring("player", "playlistInterrupt", "open", ("open", "add"))
             if setting == "open": wx.CallAfter(m3uManager.loadM3u, msg, m3uManager.REPLACE)
