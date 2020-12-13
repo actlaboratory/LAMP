32954d12516a59006031838457695ff10ab30024
Hiroki Fujii <hfujii@hisystron.com>
2020-12-13 17:31:58 +0900

    0.0.3 アップデータテスト

--- constants.py
+++ constants.py
@@ -21,7 +21,7 @@ KEYMAP_FILE_NAME="keymap.ini"
 #アプリケーション基本情報
 APP_NAME="LAMP"
 APP_FULL_NAME = "Light and Accessible Music Player"
-APP_VERSION="0.0.0"
+APP_VERSION="0.0.3"
 APP_ICON = "icon.ico"
 APP_COPYRIGHT_YEAR="2020"
 APP_LICENSE="Apache License 2.0"
