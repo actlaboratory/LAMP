bbf8cdb804dc6fa25b0759da3e6706c4e38de2db
guredora <contact@guredora.com>
2020-12-29 18:10:47 +0900

    updateに利用制限とログ機能を実装

--- update.py
+++ update.py
@@ -12,6 +12,7 @@ from views import updateDialog
 import time
 import threading
 import win32api
+from logging import getLogger
 
 def getUpdaterVersion():
 	""" return (pyVersion, exeVersion) """
@@ -32,19 +33,28 @@ def checkUpdate():
 class update(threading.Thread):
 	def __init__(self):
 		super().__init__()
+		self.log=getLogger("%s.%s" % (constants.LOG_PREFIX,"update"))
+		self.log.info("update initialized")
 		self.needStop = False
 		self.reserve = False
 
 	def update(self, auto=False):
+		self.log.info("called update auto = %s" % str(auto))
+		if not hasattr(sys, "frozen"):
+			self.log.info("update is unavailable reason: not supported")
+			if not auto:
+				simpleDialog.winDialog(_("アップデート"), _("このバージョンではアップデートを使用できません。"))
+			return
 		# アップデータチェック
 		updaterCheck = False
 		try:
 			if getUpdaterVersion()[0] == getUpdaterVersion()[1]: updaterCheck = True
 		except: pass
 		if not updaterCheck:
-			simpleDialog.dialog(_("アップデート"), _("アップデータが利用できません。お手数ですが、再度ソフトウェアをダウンロードしてください。"))
+			self.log.info("update is unavailable reason: updater not found")
+			simpleDialog.winDialog(_("アップデート"), _("アップデータが利用できません。お手数ですが、再度ソフトウェアをダウンロードしてください。"))
 			return
-		
+		self.log.info("starting to check update...")
 		params = {
 			"name": constants.APP_NAME,
 			"updater_version": constants.UPDATER_VERSION,
@@ -55,32 +65,40 @@ class update(threading.Thread):
 			response = requests.get(constants.UPDATE_URL, params, timeout = timeout)
 		except requests.exceptions.ConnectTimeout:
 			if not auto:
-				simpleDialog.dialog(_("アップデート"), _("サーバーへの通信がタイムアウトしました。"))
+				self.log.info("failed to check update reason: connection timed out")
+				simpleDialog.winDialog(_("アップデート"), _("サーバーへの通信がタイムアウトしました。"))
 			return
 		except requests.exceptions.ConnectionError as e:
 			print(e)
+			self.log.info("failed to check update reason: connection is not successful")
 			if not auto:
-				simpleDialog.dialog(_("アップデート"), _("サーバーへの接続に失敗しました。インターネット接続などをご確認ください"))
+				simpleDialog.winDialog(_("アップデート"), _("サーバーへの接続に失敗しました。インターネット接続などをご確認ください"))
 			return
 		if not response.status_code == 200:
+			self.log.info("failed to check update reason: the server returned invalid status code %d" % (response.status_code))
 			if not auto:
-				simpleDialog.dialog(_("アップデート"), _("サーバーとの通信に失敗しました。"))
+				simpleDialog.winDialog(_("アップデート"), _("サーバーとの通信に失敗しました。"))
 			return
+		self.log.info("checking update succeeded. analyzing the response from the server...")
 		self.info = response.json()
 		code = self.info["code"]
 		if code == errorCodes.UPDATER_LATEST:
+			self.log.info("update was not done reason: this is the latest version")
 			if not auto:
-				simpleDialog.dialog(_("アップデート"), _("現在のバージョンが最新です。アップデートの必要はありません。"))
+				simpleDialog.winDialog(_("アップデート"), _("現在のバージョンが最新です。アップデートの必要はありません。"))
 			return
 		elif code == errorCodes.UPDATER_BAD_PARAM:
+			self.log.info("update was not done reason: Request parameter is invalid")
 			if not auto:
-				simpleDialog.dialog(_("アップデート"), _("リクエストパラメーターが不正です。開発者まで連絡してください"))
+				simpleDialog.winDialog(_("アップデート"), _("リクエストパラメーターが不正です。開発者まで連絡してください"))
 			return
 		elif code == errorCodes.UPDATER_NOT_FOUND:
+			self.log.info("update was not done reason: not registered")
 			if not auto:
-				simpleDialog.dialog(_("アップデート"), _("アップデーターが登録されていません。開発者に連絡してください。"))
+				simpleDialog.winDialog(_("アップデート"), _("アップデーターが登録されていません。開発者に連絡してください。"))
 			return
 		elif code == errorCodes.UPDATER_NEED_UPDATE or errorCodes.UPDATER_VISIT_SITE:
+			self.log.info("opening update dialog...")
 			self.dialog = updateDialog.updateDialog()
 			self.dialog.Initialize()
 			self.dialog.Show()
@@ -93,14 +111,17 @@ class update(threading.Thread):
 	def runUpdate(self):
 		if not self.reserve: return #予約されていないアップデートはできない
 		if os.path.exists("updater.exe"):
+			self.log.info("executing updater...")
 			pid = os.getpid()
 			subprocess.Popen(("updater.exe", sys.argv[0], constants.UPDATER_WAKE_WORD, self._file_name, self.info["updater_hash"], str(pid)))
 		else:
+			self.log.info("updater not found")
 			os.remove(self._file_name)			
 			self.dialog.updater_notFound()
 		
 
 	def run(self):
+		self.log.info("downloading update file...")
 		url = self.info["updater_url"]
 		self._file_name = "update_file.zip"
 		response = requests.get(url, stream = True)
@@ -119,14 +140,14 @@ class update(threading.Thread):
 				wx.CallAfter(self.dialog.gauge.SetValue, (now_size))
 				wx.YieldIfNeeded()
 		if broken:
-			print("canceled!")
+			self.log.info("downloading update file has canceled by user")
 			os.remove(self._file_name)
 			wx.CallAfter(self.dialog.end)
 			return
-		print("downloaded!")
+		self.log.info("update file downloaded")
 		wx.CallAfter(self.dialog.end)
 		self.reserve = True
-		simpleDialog.dialog(_("アップデート"), _("ダウンロードが完了しました。\nソフトウェア終了時に、自動でアップデートされます。"))
+		simpleDialog.winDialog(_("アップデート"), _("ダウンロードが完了しました。\nソフトウェア終了時に、自動でアップデートされます。"))
 		return
 
 	def exit(self):
