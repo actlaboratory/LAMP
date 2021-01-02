41ebf6fd2ff905551b6692cbcb27b65754208012
Hiroki Fujii <hfujii@hisystron.com>
2021-01-02 23:29:41 +0900

    エクセプトフックの修正

--- lamp.py
+++ lamp.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
 #Application startup file
-# Copyright (C) 2020 Hiroki Fujii <hfujii@hisystron.com>
+# Copyright (C) 2020-2021 Hiroki Fujii <hfujii@hisystron.com>
 
 import sys, os, winsound
 #カレントディレクトリを設定
@@ -9,23 +9,34 @@ else: os.chdir(os.path.abspath(os.path.dirname(__file__)))
 
 #エラーの出力
 import traceback
+import win32event
+import simpleDialog
 
 def exchandler(type, exc, tb):
 	msg=traceback.format_exception(type, exc, tb)
 	print("".join(msg))
-	f=open("errorLog.txt", "a")
-	f.writelines(msg)
-	f.close()
+	try:
+		f=open("errorLog.txt", "a")
+		f.writelines(msg)
+		f.close()
+	except: pass
+	
+	# パイプとミューテックスの終了
+	try:
+		import lampPipe, globalVars
+		lampPipe.stopPipeServer()
+		win32event.ReleaseMutex(globalVars.app.mutex)
+	except: pass
+	
 	if hasattr(sys, "frozen") == False:
 		winsound.Beep(1500, 100)
 		winsound.Beep(1500, 100)
 		winsound.Beep(1500, 300)
 	else:
-		simpleDialog.winDialog("error", "An error has occured. Contact to the developer for further assistance.")
+		simpleDialog.winDialog("error", "An error has occured. Contact to the developer for further assistance. Detail:" + "\n".join(msg[-2:]))
 	sys.exit(-1)
 
 sys.excepthook=exchandler
-
 
 import multiprocessing
 multiprocessing.freeze_support() #これがないとマルチプロセスでおかしなことになる
@@ -45,6 +56,11 @@ import globalVars
 import m3uManager
 
 def main():
+	if os.path.exists("errorLog.txt"):
+		try:
+			os.remove("errorLog.txt")
+		except: pass
+
 	app=application.Main()
 	globalVars.app=app
 	app.initialize()
