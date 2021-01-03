e2d7fcc3a4f519c1d4a4e24398bc3293e5c9c083
Hiroki Fujii <hfujii@hisystron.com>
2021-01-04 00:47:16 +0900

    ダイアログの不具合を修正

--- startupListSetter.py
+++ startupListSetter.py
@@ -10,11 +10,11 @@ def run():
     if f != None and os.path.exists(f):
         globalVars.app.config["player"]["startupPlaylist"] = f
         d = mkDialog.Dialog("set startup playlist ok")
-        d.Initialize(_("設定完了"), _("起動時に開くプレイリストに設定しました。\n%s") %(f,), ("OK",))
+        d.Initialize(_("設定完了"), _("起動時に開くプレイリストに設定しました。\n%s") %(f,), ("OK",), sound=False)
         fxManager.confirm()
         d.Show()
     else:
         d = mkDialog.Dialog("set startup playlist error")
-        d.Initialize(_("エラー"), _("設定に失敗しました。"), ("OK",))
+        d.Initialize(_("エラー"), _("設定に失敗しました。"), ("OK",), sound=False)
         fxManager.error()
         d.Show()
