6ea19684a40b044436ee0808c3fab513a2d34aac
Kochi <c011610813@edu.teu.ac.jp>
2021-03-07 02:05:59 +0900

    キー設定ダイアログにおける親ウィンドウの指定に対応

--- simpleDialog.py
+++ simpleDialog.py
@@ -7,11 +7,15 @@ import wx
 def winDialog(title,message):
 	ctypes.windll.user32.MessageBoxW(0,message,title,0x00000040)
 
-def dialog(title,message):
-	ctypes.windll.user32.MessageBoxW(0,message,title,0x00000040)
+def dialog(title,message,parent=None):
+	if parent:
+		hwnd = parent.GetHandle()
+	else:
+		hwnd = 0
+	ctypes.windll.user32.MessageBoxW(hwnd,message,title,0x00000040)
 
-def errorDialog(message):
-	dialog = wx.MessageDialog(None,message,"error",wx.OK|wx.ICON_ERROR)
+def errorDialog(message,parent=None):
+	dialog = wx.MessageDialog(parent,message,"error",wx.OK|wx.ICON_ERROR)
 	dialog.ShowModal()
 	dialog.Destroy()
 	return
